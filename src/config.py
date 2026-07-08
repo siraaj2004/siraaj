632a3ced3cf3e77d1cb2dae2d801c5185e52d487
SHAIK MAHMMAD SIRAZ <shaiksiraaj2004@gmail.com>
2026-07-08 12:54:09 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,8 +1,14 @@
 import os
 from dotenv import load_dotenv
 
+# Load .env file
 load_dotenv()
 
+# API Keys
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+# Gmail
 EMAIL_USER = os.getenv("EMAIL_USER")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
-RECIPIENT_EMAIL = os.getenv("RECIPIENT_EMAIL")
+EMAIL_RECIPIENT = os.getenv("EMAIL_RECIPIENT")
